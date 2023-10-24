3aa6cf590f82c14b543be00f471a4cc8f51e034b
kykala Asa <kykalaasa@gmail.com>
2023-10-24 05:40:18 +0000

    money manager solution

---
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -0,0 +1,81 @@
+// Write your code here
+import './index.css'
+
+const registrationStatus = {
+  yetToRegister: 'YET_TO_REGISTER',
+  registered: 'REGISTERED',
+  registrationClosed: 'REGISTRATIONS_CLOSED',
+}
+
+const ActiveEventRegistrationDetails = props => {
+  const {ActiveEventRegistrationStatus} = props
+
+  const renderNoActiveEventView = () => (
+    <p className="no-active-event-description">
+      Click on an event, to view its registration details
+    </p>
+  )
+
+  const renderRegistrationsClosedView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+        alt="registrations closed"
+        className="registrations-closed-image"
+      />
+      <h1 className="registrations-closed-heading">
+        Registrations are closed Now!
+      </h1>
+      <p className="registrations-closed-description">
+        stay tuned. We will reopen the registrations soon!
+      </p>
+    </div>
+  )
+
+  const renderRegisteredView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png"
+        alt="registered"
+        className="registered-img"
+      />
+      <h1 className="registered-heading">
+        You have already registered for the event
+      </h1>
+    </div>
+  )
+
+  const renderYetToRegisterView = () => (
+    <div className="view-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+        alt="yet to register"
+        className="yet-to-register-image"
+      />
+      <p className="register-description">A live performance brings so much</p>
+      <button type="button" className="register-here-button">
+        Register here
+      </button>
+    </div>
+  )
+
+  const renderActiveEventRegistrationDetails = () => {
+    switch (ActiveEventRegistrationStatus) {
+      case registrationStatus.yetToRegister:
+        return renderYetToRegisterView()
+      case registrationStatus.registered:
+        return renderRegisteredView()
+      case registrationStatus.registrationClosed:
+        return renderRegistrationsClosedView()
+
+      default:
+        return renderNoActiveEventView()
+    }
+  }
+  return (
+    <div className="registration-status-container">
+      {renderActiveEventRegistrationDetails()}
+    </div>
+  )
+}
+export default ActiveEventRegistrationDetails
